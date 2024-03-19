140c5d97c11bbd7528e72fa95a7dd0520e581bb7
RimMirK <115800822+RimMirK@users.noreply.github.com>
2024-03-19 15:48:55 +0100

    Add files via upload

---
+++ plugins/translate.py
@@ -0,0 +1,50 @@
+from utils import (
+    Cmd, get_group, b, bq, code, helplist, Module, Command, Argument as Arg
+)
+
+from googletrans import Translator, LANGUAGES
+translator = Translator()
+
+cmd = Cmd(get_group())
+
+LANGS = ''
+for key, lang in LANGUAGES.items():
+    LANGS += f"{code(key)}: {lang}\n"
+
+@cmd(['tr', 'translate'])
+async def _tr(_, msg):
+    _, ln, *text = (msg.text or msg.caption).split(maxsplit=2)
+    if text == []:
+        if msg.reply_to_message:
+            text = msg.quote_text or msg.reply_to_message.text
+        else:
+            return await msg.edit("Неверный ввод данных!")
+    else:
+        text = text[0]
+    
+    try:
+        tr = translator.translate(text, ln)
+        await msg.edit(
+            f"{b(tr.src)}:\n{bq(text)}\n{b(tr.dest)}:\n{bq(tr.text)}"
+        )
+    except Exception as e:
+        await msg.edit("Error: "+str(e)+str(e.__class__.__name__))
+        
+
+@cmd('trlangs')
+async def _trlangs(_, msg):
+    await msg.edit(b("Доступные языки:\n")+LANGS) 
+    
+
+helplist.add_module(
+    Module(
+        "translate",
+        description="Google переводчик",
+        author="@RimMirK",
+        version="1.0.0",
+    ).add_command(
+        Command(['tr', 'translate'], [Arg("целевой язык"), Arg("текст/ответ")], "Перевести текст")
+    ).add_command(
+        Command(['trlangs'], [], "Список языков и их код")
+    )
+)
