e131a76279c71899e567f0bfb90297e460153ebd
RimMirK <115800822+RimMirK@users.noreply.github.com>
2024-04-03 10:20:53 +0200

    Update translate.py to v 1.1

--- plugins/translate.py
+++ plugins/translate.py
@@ -28,8 +28,26 @@ async def _tr(_, msg):
             f"{b(tr.src)}:\n{bq(text)}\n{b(tr.dest)}:\n{bq(tr.text)}"
         )
     except Exception as e:
-        await msg.edit("Error: "+str(e)+str(e.__class__.__name__))
+        await msg.edit(f"{bq(text)}\n{b(f'{e.__class__.__name__}')}: {bq(str(e))}")
         
+@cmd(['trf', 'translatefrom', 'trfrom'])
+async def _tr(_, msg):
+    _, src, ln, *text = (msg.text or msg.caption).split(maxsplit=2)
+    if text == []:
+        if msg.reply_to_message:
+            text = msg.quote_text or msg.reply_to_message.text
+        else:
+            return await msg.edit("Неверный ввод данных!")
+    else:
+        text = text[0]
+    
+    try:
+        tr = translator.translate(text, ln, src)
+        await msg.edit(
+            f"{b(tr.src)}:\n{bq(text)}\n{b(tr.dest)}:\n{bq(tr.text)}"
+        )
+    except Exception as e:
+        await msg.edit(f"{bq(text)}\n{b(f'{e.__class__.__name__}')}: {bq(str(e))}")
 
 @cmd('trlangs')
 async def _trlangs(_, msg):
@@ -41,9 +59,11 @@ helplist.add_module(
         "translate",
         description="Google переводчик",
         author="@RimMirK",
-        version="1.0.0",
+        version="1.1.0",
     ).add_command(
         Command(['tr', 'translate'], [Arg("целевой язык"), Arg("текст/ответ")], "Перевести текст")
+    ).add_command(
+        Command(['trf', 'translatefrom', 'trfrom'], [Arg("Язык оригинала"), Arg("целевой язык"), Arg("текст/ответ")], "Перевести текст")
     ).add_command(
         Command(['trlangs'], [], "Список языков и их код")
     )
